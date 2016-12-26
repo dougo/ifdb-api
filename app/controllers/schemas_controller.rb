ace6c50b839491ec985bb74f2616ee2372f7c642
Doug Orleans <dougorleans@gmail.com>
2016-12-26 01:14:07 -0500

    Pretty-print schemas.

--- app/controllers/schemas_controller.rb
+++ app/controllers/schemas_controller.rb
@@ -2,6 +2,6 @@ class SchemasController < ApplicationController
   def show
     schema = (params[:resource] + '_schema').camelize.constantize.new.schema
     schema[:id] = schema_url(params[:resource])
-    render json: schema
+    render json: JSON.pretty_generate(schema)
   end
 end
