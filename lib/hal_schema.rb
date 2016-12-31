b9e8b0f15342c40bfc93b067cb8336029803caeb
Doug Orleans <dougorleans@gmail.com>
2016-12-31 18:09:10 -0500

    Allow optional links in HALSchema.

--- lib/hal_schema.rb
+++ lib/hal_schema.rb
@@ -1,7 +1,19 @@
 class HALSchema < JSONSchema
+  class Link
+    attr_reader :name
+
+    def initialize(name, required: true)
+      @name, @required = name, required
+    end
+
+    def required?
+      @required
+    end
+  end
+
   class_attribute :_links
 
-  self._links = [:self]
+  self._links = [Link.new(:self)]
 
   def self.inherited(subclass)
     super
@@ -13,12 +25,12 @@ class HALSchema < JSONSchema
     _links
   end
 
-  def self.links(*names)
-    names.each &method(:link)
+  def self.links(*names, **opts)
+    names.each { |name| link(name, **opts) }
   end
 
-  def self.link(name)
-    _links << name
+  def self.link(name, **opts)
+    _links << Link.new(name, **opts)
   end
 
   class LinksProperty < Property
@@ -31,10 +43,14 @@ class HALSchema < JSONSchema
       @schema_class._links
     end
 
+    def required_links
+      links.select(&:required?).map(&:name)
+    end
+
     def as_json
       json = super
       json[:allOf] = [ { '$ref': 'http://hyperschema.org/mediatypes/hal#/definitions/links' },
-                       { required: @schema_class._links } ]
+                       { required: required_links } ]
       json
     end
   end
--- test/lib/hal_schema_test.rb
+++ test/lib/hal_schema_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class HALSchemaTest < ActiveSupport::TestCase
   class TestHALSchema < HALSchema
     link :foo
-    links :bar, :baz
+    links :bar, :baz, required: false
   end
 
   setup do
@@ -14,15 +14,30 @@ class HALSchemaTest < ActiveSupport::TestCase
     assert_kind_of JSONSchema, @schema
   end
 
+  test 'Link' do
+    foo = HALSchema::Link.new(:foo)
+    assert_equal :foo, foo.name
+    assert_predicate foo, :required?
+
+    bar = HALSchema::Link.new(:bar, required: false)
+    refute_predicate bar, :required?
+  end
+
   test 'links' do
     class EmptyHALSchema < HALSchema; end
-    assert_equal %i(self), EmptyHALSchema.new.links
-    assert_equal %i(self foo bar baz), @schema.links
+    assert_equal %i(self), EmptyHALSchema.new.links.map(&:name)
+    assert_equal %i(self foo bar baz), @schema.links.map(&:name)
+    assert_predicate @schema.links[0], :required?
+    assert_predicate @schema.links[1], :required?
+    refute_predicate @schema.links[2], :required?
+    refute_predicate @schema.links[3], :required?
   end
 
   test '_links property' do
-    assert_kind_of HALSchema::LinksProperty, @schema.property(:_links)
-    assert_equal @schema.links, @schema.property(:_links).links
+    prop = @schema.property(:_links)
+    assert_kind_of HALSchema::LinksProperty, prop
+    assert_equal @schema.links, prop.links
+    assert_equal %i(self foo), prop.required_links
   end
 
   test '_links is required' do
@@ -37,6 +52,6 @@ class HALSchemaTest < ActiveSupport::TestCase
     links_schemas = prop.as_json[:allOf]
     assert_equal 2, links_schemas.size
     assert_equal 'http://hyperschema.org/mediatypes/hal#/definitions/links', links_schemas[0][:$ref]
-    assert_equal %i(self foo bar baz), links_schemas[1][:required]
+    assert_equal %i(self foo), links_schemas[1][:required]
   end
 end
