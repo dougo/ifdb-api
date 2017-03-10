57de67bbbc42cfddc2331447b48269ebae0421bc
Doug Orleans <dougorleans@gmail.com>
2017-03-09 23:01:31 -0500

    Reordered Member associations to match public profile, and added posted_comments.

--- app/models/member.rb
+++ app/models/member.rb
@@ -4,15 +4,18 @@ class Member < ApplicationRecord
   has_many :comments, as: :commentable, foreign_key: :sourceid, foreign_type: :source
   has_and_belongs_to_many :games, join_table: 'gameprofilelinks', foreign_key: :userid,
                           association_foreign_key: :gameid
-  has_many :reviews, foreign_key: :userid
-  has_many :club_memberships, foreign_key: :userid
-  has_many :clubs, through: :club_memberships
   has_many :lists, class_name: 'RecommendedList', foreign_key: :userid
   has_many :polls, foreign_key: :userid
+  has_many :reviews, foreign_key: :userid
   has_and_belongs_to_many :played_games, class_name: 'Game', join_table: 'playedgames',
                           foreign_key: :userid, association_foreign_key: :gameid
   has_and_belongs_to_many :wishlist, class_name: 'Game', join_table: 'wishlists',
                           foreign_key: :userid, association_foreign_key: :gameid
   has_and_belongs_to_many :unwishlist, class_name: 'Game', join_table: 'unwishlists',
                           foreign_key: :userid, association_foreign_key: :gameid
+  has_many :club_memberships, foreign_key: :userid
+
+  # Visible on personal page only:
+
+  has_many :posted_comments, class_name: 'Comment', foreign_key: :userid
 end
--- test/models/member_test.rb
+++ test/models/member_test.rb
@@ -7,16 +7,18 @@ class MemberTest < ActiveSupport::TestCase
 
   should have_many(:comments).with_foreign_key(:sourceid) # .as(:commentable).with_foreign_type(:source)
   should have_and_belong_to_many(:games).join_table('gameprofilelinks')
-  should have_many(:reviews).with_foreign_key(:userid)
-  # TODO: discussions
-  should have_many(:club_memberships).with_foreign_key(:userid)
-  should have_many(:clubs).through(:club_memberships)
   should have_many(:lists).class_name('RecommendedList').with_foreign_key(:userid)
   should have_many(:polls).with_foreign_key(:userid)
+  should have_many(:reviews).with_foreign_key(:userid)
   should have_and_belong_to_many(:played_games).class_name('Game').join_table('playedgames')
   should have_and_belong_to_many(:wishlist).class_name('Game').join_table('wishlists')
   should have_and_belong_to_many(:unwishlist).class_name('Game').join_table('unwishlists')
+  should have_many(:club_memberships).with_foreign_key(:userid)
+
+  # TODO: discussion updates
+  # TODO: comment inbox
+  should have_many(:posted_comments).class_name('Comment').with_foreign_key(:userid)
+
   # TODO: catalog contributions
-  # TODO: user filters
   # TODO: frequent fiction points?
 end
